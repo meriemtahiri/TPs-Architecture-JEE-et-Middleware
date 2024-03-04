7da48416cfcbc1827e376851baa0864837ea1f18
TAHIRI Meriem <117281323+meriemtahiri@users.noreply.github.com>
2024-03-04 01:42:36 +0100

    basic manipulations ORM-JPA-Hibernate-SpringDta

---
+++ ORM-Jpa-Hibernate-Spring Data/src/main/java/ma/enset/ormjpahibernatespringdata/entities/Patient.java
@@ -0,0 +1,2 @@
+package ma.enset.ormjpahibernatespringdata.entities;public class Patient {
+}
---
+++ ORM-Jpa-Hibernate-Spring Data/src/main/java/ma/enset/ormjpahibernatespringdata/repositories/PatientRepo.java
@@ -0,0 +1,6 @@
+package ma.enset.ormjpahibernatespringdata.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<Product> {
+}
