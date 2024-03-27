d69087df74f53df772abb07331403d9b84bf0377
TAHIRI Meriem <117281323+meriemtahiri@users.noreply.github.com>
2024-03-27 13:26:17 +0000

    componenet-routing

---
+++ TP1_Angular/src/app/new-products/new-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-products.component.html',
+  styleUrl: './new-products.component.css'
+})
+export class NewProductsComponent {
+
+}
